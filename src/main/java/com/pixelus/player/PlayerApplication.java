f5242b1d75fe6c442ace946c9b02c2a3bff1d87a
David Mouser <David.Mouser@afl.com.au>
2015-03-06 17:24:33 +1100

    Moved hystrix configuration into the "microservice-commons" project so that it can be reused.

--- src/main/java/com/pixelus/player/PlayerApplication.java
+++ src/main/java/com/pixelus/player/PlayerApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableConfigurationProperties
+@ComponentScan({ "com.pixelus" })
 public class PlayerApplication {
 
     public static void main(String[] args) {
